061ef63268f3c63b39afcb15daf7fab7ab593eae
Marcin Borkowski <marcin.borkowski@rb.com>
2016-07-05 10:06:03 +0200

    Experiment #2

--- node-js/midi.js
+++ node-js/midi.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var midi = require('midi');
+//var mario = require('./mario.js');
 var output = new midi.output();
 
 console.log('Output device: ' + output.getPortName(0));
+//console.log(mario);
 
 try {
     output.openPort(0);
@@ -14,27 +16,28 @@ try {
 var song = [];
 
 for (var i = 0; i <= 70; i++) {
-  song.push([Math.round(Math.random()*60+40), 1200, 400]);
+  song.push([Math.round(Math.random()*60+40), 200 + Math.round(Math.random()*3000), 50 + Math.round(Math.random()*3000)]);
 }
 
 var timeline = 0;
 
 function play() {
     timeline = 0;
-    output.sendMessage([144, 60, 100]);
+    //output.sendMessage([144, 60, 100]);
     song.forEach(function (note, index) {
         let noteNo = note[0],
             duration = note[1],
             space = note[2];
 
-        //(function (noteNo, duration, space) {
             setTimeout(function () {
+                console.log('*****');
                 console.log('Playing: ' + noteNo);
                 output.sendMessage([144, noteNo, 100]);
             }, timeline);
 
             setTimeout(function () {
-                console.log('Stop: ' + noteNo + '\n\n');
+                console.log('Stop: ' + noteNo);
+                console.log('*****\n\n');
                 output.sendMessage([128, noteNo, 100]);
 
                 if (++index === song.length) {
@@ -43,7 +46,6 @@ function play() {
             }, timeline + duration);
 
             timeline += (duration + space);
-        //})(noteNo, duration, space);
     });
 }
 
